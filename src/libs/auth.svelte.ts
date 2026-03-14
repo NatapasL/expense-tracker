013f33b7a76d968e39f934742d2e9e64c1f03b47
Natapas <natapas.lue@gmail.com>
2026-03-14 22:02:49 +0700

    chore: try log

--- src/libs/auth.svelte.ts
+++ src/libs/auth.svelte.ts
@@ -56,6 +56,8 @@ class AuthState {
 	}
 
 	private setupClient() {
+		console.log(PUBLIC_VITE_GOOGLE_CLIENT_ID)
+
 		this.tokenClient = window.google.accounts.oauth2.initTokenClient({
 			client_id: PUBLIC_VITE_GOOGLE_CLIENT_ID,
 			scope:
